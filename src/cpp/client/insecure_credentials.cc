ae077126edbdf9a793e49616c9a8188ae3e70cbe
Hao Chen <chenh1024@gmail.com>
2019-07-17 12:28:15 +0800

    fix

--- src/cpp/client/insecure_credentials.cc
+++ src/cpp/client/insecure_credentials.cc
@@ -57,6 +57,7 @@ class InsecureChannelCredentialsImpl final : public ChannelCredentials {
 }  // namespace
 
 std::shared_ptr<ChannelCredentials> InsecureChannelCredentials() {
+  grpc::GrpcLibraryCodegen init;  // To call grpc_init().
   return std::shared_ptr<ChannelCredentials>(
       new InsecureChannelCredentialsImpl());
 }
